7053761abcf00145196577fdf975d7291eeb87f1
Skip Khamvongsa <skipk95@gmail.com>
2020-03-18 21:05:10 -0600

    Fixed display

--- assets/javaScript/javascript.js
+++ assets/javaScript/javascript.js
@@ -1,9 +1,9 @@
-function displayTrail() {
+// function displayTrail() {
     //ajax call for hiking API
     
-    var queryURL = `https://www.hikingproject.com/data/get-trails?${location}&maxDistance=10&key=200704227-63dce10d4323d2ac2f6a6cb59477a59e`
+    var queryURL = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200704227-63dce10d4323d2ac2f6a6cb59477a59e";
     
-    var location = $(this).attr("data-name");
+    // var location = $(this).attr("data-name");
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -11,30 +11,30 @@ function displayTrail() {
         // var results = response.data;
         console.log(response);
         console.log(response.trails[0].name);
-        for (var i = 0; i < response.length; i++) {
+        // for (var i = 0; i < response.length; i++) {
             // console.log(response.trails[0].name);
             var trailImg = $("<img>").addClass("trail");
             var gifDiv = $("<div id='gifId'>")
-            trailImg.attr("src", response.trails[i].imgSqSmall);
+            trailImg.attr("src", response.trails[0].imgSqSmall);
             // gifDiv.append(trailImg);
             $("#img-here").prepend(trailImg);
-            var trailName = $("<p>").text(response.trails[i].name);
+            var trailName = $("<p>").text(response.trails[0].name);
             $("#trail-name").append(trailName);
-            var location = $("<p>").text(response.trails[i].location);
+            var location = $("<p>").text(response.trails[0].location);
             $("#loc-here").append(location);
-            var rating = $("<p>").text(response.trails[i].stars);
+            var rating = $("<p>").text(response.trails[0].stars);
             $("#rating-here").append(rating);
-            var summary = $("<p>").text(response.trails[i].summary);
+            var summary = $("<p>").text(response.trails[0].summary);
             $("#summary-trail").append(summary);
 
-        }
+        // }
     });
-}
-$("#add-search").on("click", function() {
-    var citySearch = $("#input-search").val().trim();
-    location.push(citySearch);
-    displayTrail()
-})
+// }
+// $("#add-search").on("click", function() {
+//     var citySearch = $("#input-search").val().trim();
+//     location.push(citySearch);
+//     displayTrail()
+// })
 
 //ajax call for Beer API
 //var queryURL = " http://api.brewerydb.com/v2/?key=53ae7fd539ff1a48828cce99135a3feb";
