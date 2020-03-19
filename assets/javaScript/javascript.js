d03d2a6598aaa6b123503f2d413179e301307dce
Tammy Richardson <atrich0220@gmail.com>
2020-03-18 19:42:31 -0600

    added ajax call for hiking API

--- assets/javaScript/javascript.js
+++ assets/javaScript/javascript.js
@@ -1,1 +1,19 @@
-console.log("hello")
+//ajax call for hiking API
+var queryURL = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200704227-63dce10d4323d2ac2f6a6cb59477a59e";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
+
+//ajax call for Beer API
+//var queryURL = " http://api.brewerydb.com/v2/?key=53ae7fd539ff1a48828cce99135a3feb";
+
+//$.ajax({
+    //url: queryURL,
+    //method: "GET"
+//}).then(function (response) {
+    //console.log(response);
+//});
