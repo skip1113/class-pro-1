7b79fd9c94d16e54c5417dbf26f1eb4fa2a30978
Tammy Richardson <atrich0220@gmail.com>
2020-03-19 15:09:12 -0600

    added new brewery api. added new image to jumbotron, new card to hold brewery api data, changed text alignment and color on jumbotron

--- assets/javaScript/javascript.js
+++ assets/javaScript/javascript.js
@@ -1,34 +1,34 @@
 // function displayTrail() {
-    //ajax call for hiking API
-    
-    var queryURL = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200704227-63dce10d4323d2ac2f6a6cb59477a59e";
-    
-    // var location = $(this).attr("data-name");
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        // var results = response.data;
-        console.log(response);
-        console.log(response.trails[0].name);
-        // for (var i = 0; i < response.length; i++) {
-            // console.log(response.trails[0].name);
-            var trailImg = $("<img>").addClass("trail");
-            var gifDiv = $("<div id='gifId'>")
-            trailImg.attr("src", response.trails[0].imgSqSmall);
-            // gifDiv.append(trailImg);
-            $("#img-here").prepend(trailImg);
-            var trailName = $("<p>").text(response.trails[0].name);
-            $("#trail-name").append(trailName);
-            var location = $("<p>").text(response.trails[0].location);
-            $("#loc-here").append(location);
-            var rating = $("<p>").text(response.trails[0].stars);
-            $("#rating-here").append(rating);
-            var summary = $("<p>").text(response.trails[0].summary);
-            $("#summary-trail").append(summary);
+//ajax call for hiking API
 
-        // }
-    });
+var queryURL = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200704227-63dce10d4323d2ac2f6a6cb59477a59e";
+
+// var location = $(this).attr("data-name");
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    // var results = response.data;
+    console.log(response);
+    console.log(response.trails[0].name);
+    // for (var i = 0; i < response.length; i++) {
+    // console.log(response.trails[0].name);
+    var trailImg = $("<img>").addClass("trail");
+    var gifDiv = $("<div id='gifId'>")
+    trailImg.attr("src", response.trails[0].imgSqSmall);
+    // gifDiv.append(trailImg);
+    $("#img-here").prepend(trailImg);
+    var trailName = $("<p>").text(response.trails[0].name);
+    $("#trail-name").append(trailName);
+    var location = $("<p>").text(response.trails[0].location);
+    $("#loc-here").append(location);
+    var rating = $("<p>").text(response.trails[0].stars);
+    $("#rating-here").append(rating);
+    var summary = $("<p>").text(response.trails[0].summary);
+    $("#summary-trail").append(summary);
+
+    // }
+});
 // }
 // $("#add-search").on("click", function() {
 //     var citySearch = $("#input-search").val().trim();
@@ -37,11 +37,33 @@
 // })
 
 //ajax call for Beer API
-//var queryURL = " http://api.brewerydb.com/v2/?key=53ae7fd539ff1a48828cce99135a3feb";
+var queryURL = "https://api.openbrewerydb.org/breweries?by_city=colorado_springs";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (result) {
+    console.log(result);
+    console.log(result[1].name);
+    // for (var i = 0; i < response.length; i++) {
+    // console.log(response.trails[0].name);
+    //var trailImg = $("<img>").addClass("trail");
+    //var gifDiv = $("<div id='gifId'>")
+    //trailImg.attr("src", response.trails[0].imgSqSmall);
+    // gifDiv.append(trailImg);
+    //$("#img-here").prepend(trailImg);
+    var breweryName = $("<p>").text(result[1].name);
+    $("#brewery-name").append(breweryName);
 
-//$.ajax({
-    //url: queryURL,
-    //method: "GET"
-//}).then(function (response) {
-    //console.log(response);
-//});
+    var street = $("<p>").text(result[1].street);
+    $("#street-here").append(street);
+
+    var brewery_type = $("<p>").text(result[1].brewery_type);
+    $("#brewery-type").append(brewery_type);
+
+    var phone = $("<p>").text(result[1].phone);
+    $("#phone-here").append(phone);
+
+    var website_url = $("<p>").text(result[1].website_url);
+    $("#website-here").append(website_url);
+
+});
