eb72dd7d6853006b04c6e05d4b764e8f7761b49e
Skip Khamvongsa <skipk95@gmail.com>
2020-10-17 15:49:22 -0600

    Created config, gitignore, env, passport files and boilerplate

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: "rootroot",
+    database: "trails_tap",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    // eslint-disable-next-line camelcase
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
